9bac22a95935d9f8ad4da851a0d519f3e02ca2c1
rphmeier <rphmeier@gmail.com>
2015-07-10 20:56:59 -0400

    Moved world into separate module.

--- src/ecs/mod.rs
+++ src/ecs/mod.rs
@@ -37,14 +37,15 @@
 //! ```
 
 use std::any::{Any, TypeId};
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::mem;
-use std::ops::{Deref, DerefMut};
 use std::raw::TraitObject;
 
 pub use self::vec_mapper::VecMapper;
+pub use self::world::{World, WorldBuilder};
 
 pub mod vec_mapper;
+pub mod world;
 
 /// An unique entity.
 /// Entities each have a unique id which serves
@@ -164,176 +165,6 @@ pub trait System {
     /// These entities will all implement the traits
     /// this system was registered with.
     fn process(&mut self, &[Entity], &mut ComponentMappers);
-}
-
-// Helper function for system processing.
-// Returns a vector of all values in v which are duplicate at least n times.
-#[inline]
-fn duplicate_n_times<T: Ord + Eq + Clone>(v: Vec<T>, n: usize) -> Vec<T> {
-    if v.len() == 0 { return v }
-    else if n == 0 { return Vec::new() }
-
-    let mut v = v;
-    let mut duplicates = Vec::new();
-
-    v.sort_by(|a, b| a.cmp(b));
-
-    let mut i = 1;
-    let mut prev_val = &v[0];
-    let mut count = 1;
-    while i < v.len() {
-        let cur_val = &v[i];
-        if *prev_val == *cur_val {
-            count += 1;
-        } else {
-            prev_val = cur_val;
-            count = 1;
-        }
-
-        if count == n { duplicates.push(cur_val.clone()) } 
-
-        i += 1;
-    }
-
-    duplicates
-}
-
-/// The world ties together entities, components, and systems.
-pub struct World {
-    next: Entity,
-    entities: HashSet<Entity>,
-    component_mappers: ComponentMappers,
-    systems: Vec<Box<System>>,
-}
-
-impl World {
-    /// Process all the systems in this world in arbitrary order.
-    pub fn process_systems(&mut self) {
-        for sys in &mut self.systems {
-            let comps = sys.dependent_components();
-            let num_components = comps.len();
-            if num_components == 0 { continue; }
-            // Find entities which contain requisite component types.
-            let mut entities = Vec::new();
-            for id in &comps {
-                entities.append(
-                    &mut self.component_mappers.get_handle(id).unwrap().entities()
-                );
-            }
-            if num_components > 1 {
-                entities = duplicate_n_times(entities, num_components);
-            }
-            sys.process(&entities, &mut self.component_mappers);
-        }
-    }
-
-    /// Creates an entity.
-    pub fn next_entity(&mut self) -> Entity {
-        while self.is_alive(self.next) {
-            self.next.id += 1;
-        }
-        self.entities.insert(self.next);
-        self.next
-    }
-
-    /// Creates an entity, initializing it with a prototype.
-    pub fn next_entity_prototyped<P: Prototype>(&mut self, proto: &P) -> Entity {
-        let e = self.next_entity();
-        proto.initialize(e, &mut self.component_mappers);
-        e
-    }
-
-    /// Creates a vector of n entities.
-    pub fn next_entities(&mut self, n: usize) -> Vec<Entity> {
-        let mut entities = Vec::with_capacity(n);
-        for _ in 0..n {
-            entities.push(self.next_entity());
-        }
-        entities
-    }
-
-    /// Creates n entities, initialized with a prototype.
-    pub fn next_entities_prototyped<P>(&mut self, n: usize, proto: P) -> Vec<Entity>
-    where P: Prototype {
-        let entities = self.next_entities(n);
-        for e in &entities {
-            proto.initialize(*e, &mut self.component_mappers);
-        }
-        entities
-    }
-
-
-    /// Whether an entity is currently "alive", or exists.
-    pub fn is_alive(&self, e: Entity) -> bool {
-        self.entities.contains(&e)
-    }
-
-    /// Destroy an entity.
-    pub fn destroy_entity(&mut self, e: Entity) {
-        self.entities.remove(&e);
-        for (_, h) in self.component_mappers.0.iter_mut() {
-            h.handle.remove(e);
-        }
-    }
-}
-
-impl Deref for World {
-    type Target = ComponentMappers;
-    fn deref(&self) -> &ComponentMappers {
-        &self.component_mappers
-    }
-}
-
-impl DerefMut for World {
-    fn deref_mut(&mut self) -> &mut ComponentMappers {
-        &mut self.component_mappers
-    }
-}
-
-/// Factory for `World`. 
-pub struct WorldBuilder {
-    component_mappers: ComponentMappers,
-    systems: Vec<Box<System>>,
-}
-
-impl WorldBuilder {
-    /// Create a new `WorldBuilder`.
-    pub fn new() -> WorldBuilder {
-        WorldBuilder { 
-            component_mappers: ComponentMappers(HashMap::new()),
-            systems: Vec::new() 
-        }
-    }
-
-    /// Add a component mapper to this world.
-    pub fn with_component_mapper<T, M>(self, mapper: M) -> WorldBuilder
-    where T: Component, M: ComponentMapper<Component=T> + Any {
-        let mut s = self;
-        s.component_mappers.0.insert(
-            M::Component::id(),
-            MapperHandle::from_mapper(mapper)
-        );
-        s
-    }
-
-    /// Add a system to this world.
-    pub fn with_system<S>(self, sys: S) -> WorldBuilder
-    where S: System + 'static {
-        let mut s = self;
-        s.systems.push(Box::new(sys));
-        s
-    }
-
-    /// Builds a world from self.
-    pub fn build(self) -> World {
-        World {
-            next: Entity { id: 0 },
-            entities: HashSet::new(),
-            component_mappers: self.component_mappers,
-            systems: self.systems
-        }
-    }
-
 }
 
 #[cfg(test)]

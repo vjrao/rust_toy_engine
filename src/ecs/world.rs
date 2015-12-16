cebb05c72fb681659db264784774f95fd7df2a86
rphmeier <rphmeier@gmail.com>
2015-12-16 13:00:16 -0500

    Added some component-related type lists.

---
+++ src/ecs/world.rs
@@ -0,0 +1,36 @@
+use memory::allocator::{self, Address, Allocator, DefaultAllocator, Kind};
+// The main allocator: guaranteed to live as long as the world itself.
+#[derive(Clone, Copy)]
+pub struct MainAllocator(DefaultAllocator);
+
+impl MainAllocator {
+	pub fn new() -> Self {
+		MainAllocator(DefaultAllocator)
+	}
+}
+
+// forwarding impl for allocator
+unsafe impl Allocator for MainAllocator {
+	type Error = <DefaultAllocator as Allocator>::Error;
+	
+	#[inline]
+	unsafe fn alloc(&mut self, kind: Kind) -> Result<Address, Self::Error> {
+		self.0.alloc(kind)
+	}
+	
+	#[inline]
+	unsafe fn dealloc(&mut self, addr: Address, kind: Kind) -> Result<(), Self::Error> {
+		self.0.dealloc(addr, kind)
+	}
+	
+	#[inline]
+	unsafe fn usable_size(&self, kind: Kind) -> (allocator::Capacity, allocator::Capacity) {
+		self.0.usable_size(kind)
+	}
+	
+	#[inline]
+	unsafe fn realloc(&mut self, addr: Address, kind: Kind, new_kind: Kind) -> Result<Address, Self::Error> {
+		self.0.realloc(addr, kind, new_kind)
+	}
+	
+}

53d8910790ae3ce3d1e30ae2b2f6cb59c6bfbd51
rphmeier <rphmeier@gmail.com>
2015-12-17 21:56:11 -0500

    Added test stub for WorldAllocator as DefaultAllocator.

--- src/ecs/world.rs
+++ src/ecs/world.rs
@@ -1,16 +1,18 @@
 use memory::allocator::{self, Address, Allocator, DefaultAllocator, Kind};
-// The main allocator: guaranteed to live as long as the world itself.
-#[derive(Clone, Copy)]
-pub struct MainAllocator(DefaultAllocator);
 
-impl MainAllocator {
-	pub fn new() -> Self {
-		MainAllocator(DefaultAllocator)
-	}
-}
+use super::component::{Component, ComponentMap, ComponentMaps};
 
-// forwarding impl for allocator
-unsafe impl Allocator for MainAllocator {
+/// The world's allocator: for long term storage.
+///
+/// For now, this just forwards to the heap.
+#[cfg(not(test))] #[derive(Clone, Copy)]
+pub struct WorldAllocator(DefaultAllocator);
+
+#[cfg(test)]
+pub type WorldAllocator = DefaultAllocator;
+
+#[cfg(not(test))]
+unsafe impl Allocator for WorldAllocator {
 	type Error = <DefaultAllocator as Allocator>::Error;
 	
 	#[inline]
@@ -24,13 +26,17 @@ unsafe impl Allocator for MainAllocator {
 	}
 	
 	#[inline]
-	unsafe fn usable_size(&self, kind: Kind) -> (allocator::Capacity, allocator::Capacity) {
-		self.0.usable_size(kind)
-	}
-	
-	#[inline]
 	unsafe fn realloc(&mut self, addr: Address, kind: Kind, new_kind: Kind) -> Result<Address, Self::Error> {
 		self.0.realloc(addr, kind, new_kind)
 	}
 	
+	#[inline]
+	unsafe fn usable_size(&self, kind: Kind) -> (allocator::Capacity, allocator::Capacity) {
+		self.0.usable_size(kind)
+	}
+}
+
+#[derive(Clone)]
+pub struct World<C: ComponentMaps> {
+	components: C,
 }

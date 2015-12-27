f3d1559a0b217b6b319d10f690a2436dd9e6b0b3
Robert Habermeier <rphmeier@gmail.com>
2015-12-27 03:46:10 -0500

    Implemented EntityManager synchronously.

--- src/ecs/entity.rs
+++ src/ecs/entity.rs
@@ -1,7 +1,9 @@
 use memory::collections::{VecDeque, Vector};
 
-
+use std::cell::RefCell;
 use std::slice;
+
+use util::sync::RwSpinLock;
 
 use super::world::WorldAllocator;
 
@@ -18,6 +20,9 @@ const INDEX_BITS: usize = (64 - GEN_BITS);
 const INDEX_MASK: usize = (1 << INDEX_BITS) - 1;
 // The default value for min_unused. 
 const DEFAULT_MIN_UNUSED: usize = 1024;
+
+// The amount of entities to iterate over before checking for writers.
+const ITERS_BEFORE_RELOCKING: usize = 128;
 
 /// An unique entity.
 /// Entities each have a unique id which serves
@@ -47,15 +52,20 @@ pub fn generation_of(e: Entity) -> u8 {
         ((e.id >> INDEX_BITS) & GEN_MASK) as u8
 }
 
+type GenVec = Vector<u8, WorldAllocator>;
+type Unused = VecDeque<usize, WorldAllocator>;
+
 /// Manages the creation and destruction of entities.
+/// This is fully thread-safe.
 pub struct EntityManager {
-    generation: Vector<u8, WorldAllocator>,
-    unused: VecDeque<usize, WorldAllocator>,
+    generation: RwSpinLock<GenVec>,
+    unused: RefCell<Unused>,
     min_unused: usize,
 }
 
 impl EntityManager {
     /// Create a new entity manager.
+    #[inline]
     pub fn new(alloc: WorldAllocator) -> Self {
         EntityManager::with_min_unused(alloc, DEFAULT_MIN_UNUSED)
     }
@@ -65,22 +75,18 @@ impl EntityManager {
     /// any are recycled.
     pub fn with_min_unused(alloc: WorldAllocator, min_unused: usize) -> Self {
         EntityManager {
-            generation: Vector::with_alloc(alloc),
-            unused: VecDeque::with_alloc_and_capacity(alloc, min_unused + 1),
+            generation: RwSpinLock::new(GenVec::with_alloc(alloc)),
+            unused: RefCell::new(Unused::with_alloc_and_capacity(alloc, min_unused + 1)),
             min_unused: min_unused,
         }
     }
 
     /// Creates an entity.
     pub fn next_entity(&mut self) -> Entity {
-        if self.unused.len() <= self.min_unused {
-            self.generation.push(0);
-            let idx = self.generation.len() - 1;
-            Entity::new(idx, 0)
-        } else {
-            let idx = self.unused.pop_front().unwrap();
-            Entity::new(idx, self.generation[idx])
-        }
+        let mut generation = self.generation.write().unwrap();
+        let mut unused = self.unused.borrow_mut();
+       
+        make_entity(&mut *generation, &mut *unused, self.min_unused)
     }
 
     /// Creates n entities and puts them into the slice given.
@@ -88,53 +94,43 @@ impl EntityManager {
     ///
     /// Returns the number of entities created.
     pub fn next_entities(&mut self, buf: &mut [Entity], n: usize) -> usize {
+        let mut generation = self.generation.write().unwrap();
+        let mut unused = self.unused.borrow_mut();
         let num = ::std::cmp::min(n, buf.len());
+        
         for i in 0..num {
-            buf[i] = self.next_entity();
+            buf[i] = make_entity(&mut *generation, &mut *unused, self.min_unused);
         }
+        
         num
     }
 
     /// Whether an entity is currently "alive", or exists.
     pub fn is_alive(&self, e: Entity) -> bool {
-        self.generation[index_of(e)] == generation_of(e)
+        self.generation.read().unwrap()[index_of(e)] == generation_of(e)
     }
 
     /// Destroy an entity.
     pub fn destroy_entity(&mut self, e: Entity) {
+        let mut gen = self.generation.write().unwrap();
         let idx = index_of(e);
-        self.generation[idx] += 1;
-        self.unused.push_back(idx);
-    }
-
-    /// Iterate over all living entities
-    pub fn entities(&self) -> Entities {
-        Entities {
-            index: 0,
-            generation: self.generation.iter(),
-        }
+        gen[idx] += 1;
+        self.unused.borrow_mut().push_back(idx);
     }
     
     /// Get an upper bound on the number of entities which could be live.
     pub fn size_hint(&self) -> usize {
-        self.generation.len()
+        self.generation.read().unwrap().len()
     }
 }
 
-/// An iterator over entities.
-pub struct Entities<'a> {
-    index: usize,
-    generation: slice::Iter<'a, u8>,
-}
-
-impl<'a> Iterator for Entities<'a> {
-    type Item = Entity;
-
-    fn next(&mut self) -> Option<Entity> {
-        self.generation.next().map(|gen| {
-            let e = Entity::new(self.index, *gen);
-            self.index += 1;
-            e
-        })
+fn make_entity(gen: &mut GenVec, unused: &mut Unused, min_unused: usize) -> Entity {
+    if unused.len() <= min_unused {
+        gen.push(0);
+        let idx = gen.len() - 1;
+        Entity::new(idx, 0)
+    } else {
+        let idx = unused.pop_front().unwrap();
+        Entity::new(idx, gen[idx])
     }
 }
